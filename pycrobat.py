6242a2266e77d1ba32495d0818627119c257394c
Simone <ceciresimone@itis-molinari.eu>
2025-08-15 12:46:16 +0200

    added extract and split

--- pycrobat.py
+++ pycrobat.py
@@ -1,5 +1,5 @@
-from PyPDF2 import PdfWriter
 import os
+from PyPDF2 import PdfReader, PdfWriter
 
 exit = False
 
@@ -36,5 +36,73 @@ while not exit:
                 print("Provide 2 or more files")
             else:
                 print("Files not provided")
+    elif "split " in command.lower():
+        command_splitted = command.replace("split ", "").split(" ")
+        error = None
+
+        filename = command_splitted[0]
+        step = int(command_splitted[1])
+
+        if not os.path.isfile(filename):
+            error = f"'{filename}' is not a valid file"
+        
+        reader = PdfReader(filename)
+
+        if step <= 0 or step > len(reader.pages):
+            error = f"Step '{step}' is not valid"
+        
+        if error:
+            print(error)
+        else:
+            writer = PdfWriter()
+            printed = 0
+            filename = filename.replace(".pdf", "")
+
+            for i in range(len(reader.pages)):
+                writer.add_page(reader.pages[i])
+                
+                if (i + 1) % step == 0:
+                    printed += 1
+                    with open(f"{filename}{printed}.pdf", "wb") as output:
+                        writer.write(output)
+                    writer = PdfWriter()
+                elif (i + 1) == len(reader.pages):
+                    printed += 1
+                    with open(f"{filename}{printed}.pdf", "wb") as output:
+                        writer.write(output)
+            print("File splitted")
+    elif "extract " in command.lower():
+        command_splitted = command.replace("extract ", "").split(" ")
+        error = None
+
+        filename = command_splitted[0]
+        pages_str = command_splitted[1]
+
+        if not os.path.isfile(filename):
+            error = f"'{filename}' is not a valid file"
+        
+        reader = PdfReader(filename)
+
+        pages = pages_str.split("-")
+        if len(pages) == 1:
+            pages.append(pages[0])
+        
+        if len(pages) > 2:
+            error = "Pages are not valid"
+
+        if error:
+            print(error)
+        else:
+            writer = PdfWriter()
+            filename = filename.replace(".pdf", "")
+   
+            start = int(pages[0]) - 1
+            end = int(pages[1]) - 1 if pages[1] != '' else len(reader.pages) - 1
+
+            for i in range(start, end + 1):
+                writer.add_page(reader.pages[i])
+
+            with open(f"{filename}{(str(start + 1))}-{str(end + 1) if start != end else start + 1}.pdf", "wb") as output:
+                writer.write(output)
     else:
         print("Invalid command")
